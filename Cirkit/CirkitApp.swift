959c6e61ecdb043da3640277a4001d7dc99f8f33
Barış Akçay <embarisakcay@gmail.com>
2026-03-02 16:28:57 +0300

    Initial Commit

---
+++ Cirkit/CirkitApp.swift
@@ -0,0 +1,32 @@
+//
+//  CirkitApp.swift
+//  Cirkit
+//
+//  Created by Baris Akcay on 2.03.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CirkitApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CirkitTests/CirkitTests.swift
@@ -0,0 +1,17 @@
+//
+//  CirkitTests.swift
+//  CirkitTests
+//
+//  Created by Baris Akcay on 2.03.2026.
+//
+
+import Testing
+@testable import Cirkit
+
+struct CirkitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
